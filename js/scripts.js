2185d9c86cfdb5b417c4eadd850a0038e76b6f17
kingsofcoding <simplecooldude1@gmail.com>
2016-08-24 00:09:55 +0000

    delete everything

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 document.getElementById("throw").addEventListener("keyup", function() {
    var words = document.getElementById("throw").innerHTML.split(" ");
-   for(var i = 0; i < words.length; i++) {
-       if(words[i])
-   }
+   
 });
