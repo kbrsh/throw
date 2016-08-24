dbad128c9b8d431aed6487510a8fb3c341c590c0
kingsofcoding <simplecooldude1@gmail.com>
2016-08-24 00:05:43 +0000

    use innerHTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 document.getElementById("throw").addEventListener("keyup", function() {
-   var words = document.getElementById("throw").split(" ");
+   var words = document.getElementById("throw").innerHTML.split(" ");
    for(var i = 0; i < words.length; i++) {
-       
+       if(words[i])
    }
 });
