4463564a4f2e6b78404953a77c9391ec54be8b3a
kingsofcoding <simplecooldude1@gmail.com>
2016-08-24 02:37:57 +0000

    cname for domain

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@ document.getElementById("throw").addEventListener("keyup", function() {
    
 });
 
+document.getElementById("throw").innerHTML = window.location.href.split("/#")[1] || "This is your Throw. \nIt is a simple note, that supports basic markdown syntax.\nThe best part? It is all stored in the URL."
+
+
+
+
 function markdown(text) {
     //return text.replace(/\[(.*?)\]\((.*?)\)/ig, '<a href=\'$2\'>$1</a>');
     // for now
