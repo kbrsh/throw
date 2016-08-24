cc592b1b96fe220c3a2e6eed1687c7bb5f450774
kingsofcoding <simplecooldude1@gmail.com>
2016-08-24 00:32:08 +0000

    basic markdown commetn

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 document.getElementById("throw").addEventListener("keyup", function() {
-   var words = document.getElementById("throw").innerHTML.split(" ");
+   this.innerHTML = markdown(this.innerHTML);
    
 });
+
+function markdown(text) {
+    //return text.replace(/\[(.*?)\]\((.*?)\)/ig, '<a href=\'$2\'>$1</a>');
+    // for now
+    return text;
+}
