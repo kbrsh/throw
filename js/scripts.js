a6a3cb8a09f74e84f1b0c95052cf5724c2b9657a
kingsofcoding <simplecooldude1@gmail.com>
2016-08-24 04:42:04 +0000

    use marka

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,9 @@ var init = function() {
 
 
 
-function markdown(text) {
-    //return text.replace(/\[(.*?)\]\((.*?)\)/ig, '<a href=\'$2\'>$1</a>');
-    // for now
-    return text;
-}
-
 init();
 
 document.getElementById("throw").addEventListener("keyup", function() {
-   this.innerHTML = markdown(this.innerHTML);
+   this.innerHTML = marka(this.innerHTML);
    window.location.href="/#" + btoa(this.innerHTML);
 });
