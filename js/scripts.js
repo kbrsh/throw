31685f2823eefe2fe9d1d4a34329fd21669a8dbb
kingsofcoding <simplecooldude1@gmail.com>
2016-08-24 02:41:32 +0000

    functional programming

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 var init = function() {
     document.getElementById("throw").innerHTML = atob(window.location.href.split("/#")[1]) || "This is your Throw. \nIt is a simple note, that supports basic markdown syntax.\nThe best part? It is all stored in the URL."
 }
@@ -10,4 +9,10 @@ function markdown(text) {
     // for now
     return text;
 }
+
+init();
+
+document.getElementById("throw").addEventListener("keyup", function() {
+   this.innerHTML = markdown(this.innerHTML);
+});
 
