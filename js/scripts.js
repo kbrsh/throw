c08de0f77b583cf2d61d6e012dbea98c86df6fc2
Kabir Shah <iamkbrsh@gmail.com>
2017-11-22 07:42:54 -0800

    Cleanup + Fix for gh domain (fixes #6)

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,8 @@
-var init = function() {
-    document.getElementById("throw").innerHTML = window.location.href.split("/#")[1] ? atob(window.location.href.split("/#")[1]) : "This is your Throw. \nIt is a simple note.\nThe best part? It is all stored in the URL."
-    window.location.href="/#" + btoa(document.getElementById("throw").innerHTML);
-}
+var throw = document.getElementById("throw")
 
-
-
-init();
+throw.innerHTML = window.location.hash.length === 0 ? "This is your Throw. \nIt is a simple  throwaway note that is stored in the URL." : atob(window.location.hash);
+window.location.href="/#" + btoa(document.getElementById("throw").innerHTML);
 
 document.getElementById("throw").addEventListener("keyup", function() {
-   window.location.href="/#" + btoa(this.innerHTML);
-});
-
-document.getElementById("throw").addEventListener("blur", function() {
-    this.innerHTML = this.innerHTML;
+   window.location.hash = btoa(encodeURI(this.innerHTML));
 });
