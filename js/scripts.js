3609c9b4dda037d62288b09cad9f1fb4d9796abb
Kabir Shah <iamkbrsh@gmail.com>
2017-11-22 08:15:22 -0800

    correctly decode

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var throwEl = document.getElementById("throw");
 
-throwEl.innerHTML = window.location.hash.length === 0 ? "This is your Throw. \nIt is a simple  throwaway note that is stored in the URL." : decodeURI(atob(window.location.hash));
+throwEl.innerHTML = window.location.hash.length === 0 ? "This is your Throw. \nIt is a simple  throwaway note that is stored in the URL." : decodeURI(atob(window.location.hash.substring(1)));
 window.location.hash = encodeURI(btoa(throwEl.innerHTML));
 
 throwEl.addEventListener("keyup", function() {
