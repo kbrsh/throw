e4d209c421b331d74109d6b10f3031572337b865
Kabir Shah <iamkbrsh@gmail.com>
2017-11-22 08:07:06 -0800

    use hash

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-var throwEl = document.getElementById("throw")
+var throwEl = document.getElementById("throw");
 
 throwEl.innerHTML = window.location.hash.length === 0 ? "This is your Throw. \nIt is a simple  throwaway note that is stored in the URL." : atob(window.location.hash);
-window.location.href="/#" + btoa(throwEl.innerHTML);
+window.location.hash = btoa(encodeURI(throwEl.innerHTML));
 
 throwEl.addEventListener("keyup", function() {
    window.location.hash = btoa(encodeURI(throwEl.innerHTML));
