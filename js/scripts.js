d8eababd81b96c610235f29145075d806cb6bafd
Kabir Shah <iamkbrsh@gmail.com>
2017-11-22 07:44:36 -0800

    Valid js keyword

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-var throw = document.getElementById("throw")
+var throwEl = document.getElementById("throw")
 
-throw.innerHTML = window.location.hash.length === 0 ? "This is your Throw. \nIt is a simple  throwaway note that is stored in the URL." : atob(window.location.hash);
-window.location.href="/#" + btoa(document.getElementById("throw").innerHTML);
+throwEl.innerHTML = window.location.hash.length === 0 ? "This is your Throw. \nIt is a simple  throwaway note that is stored in the URL." : atob(window.location.hash);
+window.location.href="/#" + btoa(throwEl.innerHTML);
 
-document.getElementById("throw").addEventListener("keyup", function() {
-   window.location.hash = btoa(encodeURI(this.innerHTML));
+throwEl.addEventListener("keyup", function() {
+   window.location.hash = btoa(encodeURI(throwEl.innerHTML));
 });
